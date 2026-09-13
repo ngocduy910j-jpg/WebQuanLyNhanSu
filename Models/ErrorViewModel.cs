2282ca155713690785ce659053886d9966294090
ngocduy910j-jpg <ngocduy910j@gmail.com>
2026-09-13 22:55:28 +0700

    new model

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebQuanLyNhanSu.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
